fc1a72ac8a5b8933b5e4bfdc3ec4c1c081ca6ce5
Carlos <cdmejia4@utpl.edu.ec>
2025-04-16 22:46:43 -0500

    commit final app cargada y renderizada

--- app.py
+++ app.py
@@ -4,49 +4,46 @@ import requests
 
 app = Flask(__name__)
 
-
 @app.route('/')
 def home():
     actual = datetime.now()
-    fecha_formateada = actual.strftime("%d, %B, %Y, %H, %M, %S")
+    fecha_formateada = actual.strftime("%d de %B de %Y, %H:%M:%S")
 
     url = "https://gist.githubusercontent.com/reroes/502d11c95f1f8a17d300ece914464c57/raw/872172ebb60e22e95baf8f50e2472551f49311ff/gistfile1.txt"
 
     try:
-        response = requests.get(url)
-        # response.status_code
+        response = requests.get(url) # obtener una respuesta http en base a la url
         response.raise_for_status()
-
         contenido = response.text
-
         lineas = contenido.strip().split('\n')
 
-        personas = []
+        personas_filtradas = []
+        primera_linea = True # ignorar encabezado
 
         for linea in lineas:
             if linea.strip():
-                partes = linea.split(';')
-                if len(partes) >= 3:
+                if primera_linea and "id" in linea.lower():
+                    primera_linea = False
+                    continue
+
+                partes = linea.split('|')
+
+                if len(partes) >= 5:
                     id_persona = partes[0].strip()
-                    # Verificar si el ID comienza con 3, 4, 5 o 7
                     if id_persona.startswith(('3', '4', '5', '7')):
-                        nombre = partes[1].strip()
-                        correo = partes[2].strip()
-                        personas.append({
+                        persona = { # se crea el diccionario
                             'id': id_persona,
-                            'nombre': nombre,
-                            'correo': correo
-                        })
+                            'nombre': partes[1].strip(),
+                            'apellido': partes[2].strip(),
+                            'pais': partes[3].strip(),
+                            'direccion': partes[4].strip()
+                        }
+                        personas_filtradas.append(persona) # se agrega a la lista
 
-        return render_template('index.html',
-                               fecha=fecha_formateada,
-                               personas=personas)
-
-
+        return render_template('index.html', fecha=fecha_formateada, personas=personas_filtradas)
 
     except Exception as e:
         return f"Error al procesar el archivo: {str(e)}"
 
-
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app.run(debug=True)
